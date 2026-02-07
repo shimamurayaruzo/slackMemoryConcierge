004550c69cbbdbd59d73bbf7bcd433a3323bb4f6
slackMemoryConcierge <slackMemoryConcierge@example.com>
2026-02-07 16:07:02 +0900

    Use MEMU_USER_ID fallback for team id

--- app/config.py
+++ app/config.py
@@ -8,7 +8,8 @@ class Settings:
         self.google_drive_folder_id = os.environ.get("GOOGLE_DRIVE_FOLDER_ID")
         self.google_credentials_path = os.environ.get("GOOGLE_API_CREDENTIALS")
         self.memu_api_key = os.environ.get("MEMU_API_KEY")
-        self.memu_team_id = os.environ.get("MEMU_TEAM_ID", "slack_team")
+        self.memu_user_id = os.environ.get("MEMU_USER_ID")
+        self.memu_team_id = os.environ.get("MEMU_TEAM_ID") or self.memu_user_id or "slack_team"
         self.memu_agent_id = os.environ.get("MEMU_AGENT_ID", "slack-doc-bot")
         self.port = int(os.environ.get("PORT", "8000"))
 
